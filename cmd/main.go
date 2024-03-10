6fdd223b434184afcc4733bdb88e5cf06ec476a3
Olga Aysina <olyaaisina@yandex.ru>
2024-03-10 17:37:52 +0500

    структура проекта + http сервер

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"github.com/vkorneva/todo-app"
+	"github.com/vkorneva/todo-app/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
 
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http sever: %s", err.Error())
 	}
 }
